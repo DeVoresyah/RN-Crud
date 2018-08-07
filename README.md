335b00cdb52a4060dd334f4ca80f2e0367b7d7f1
DeVoresyah ArEst <de.voresyah@gmail.com>
2018-08-07 15:22:22 +0700

    initial commit :rocket:

---
+++ README.md
@@ -0,0 +1,42 @@
+![License][license-image]
+
+## Backend Server for The App
+This is backend server for the app using expressjs
+
+## Installation
+open terminal and change directory to your desired folder, then:
+```
+$ git clone -b server https://github.com/DeVoresyah/RN-Crud YourServer
+$ cd YourServer
+$ npm install
+```
+
+## Running Your App
+Make sure your mysql already running, then run the server
+```
+$ npm run dev // for development
+or
+$ npm run start // for production
+```
+
+## Database
+Change database configuration on `/lib/conDB.js`
+
+## Find me
+You can find me on social media xD
+- [Facebook][facebook]
+- [Instagram][instagram]
+- [Linkedin][linkedin]
+
+and also [Fiverr][fiverr]
+
+## License
+The code is available at [GitHub][home] under the [MIT license][license].
+
+[home]: https://github.com/DeVoresyah/RN-Crud
+[license-image]: https://img.shields.io/badge/license-MIT-blue.svg
+[license]: http://revolunet.mit-license.org
+[facebook]: https://facebook.com/DeVoresyah
+[instagram]: https://instagram.com/devoresyah
+[linkedin]: https://linkedin.com/in/devoresyah
+[fiverr]: https://www.fiverr.com/devoresyah
---
+++ routes/contact.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var conDB = require('../lib/conDB');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  conDB.query('SELECT * FROM contact', function(err, rows, fields) {
+  	if (err) {
+  		throw err
+  	}
+
+  	res.send(rows);
+  });
+});
+
+router.post('/add', function(req, res, next) {
+	var data = req.body;
+	conDB.query('INSERT INTO contact (name, email, phone) VALUES ("'+data.name+'", "'+data.email+'", "'+data.phone+'")', function(err, rows, fields) {
+		if (err) {
+			throw err
+		}
+
+		res.send({
+			msg: "Contact has been added!"
+		});
+	});
+});
+
+router.put('/update', function(req, res, next) {
+	var id = req.body.id;
+	var data = req.body;
+	conDB.query('UPDATE contact SET name="'+data.name+'", email="'+data.email+'", phone="'+data.phone+'" WHERE id='+id, function(err, rows, fields) {
+		if (err) {
+			throw err
+		}
+		res.send({
+			msg: "Contact has been updated!"
+		});
+	});
+});
+
+router.delete('/delete/:id', function(req, res, next) {
+	var id = req.params.id;
+	conDB.query('DELETE FROM contact WHERE id='+id, function(err, rows, fields) {
+		if (err) {
+			throw err
+		}
+		res.send({
+			msg: "Contact has been deleted!"
+		});
+	});
+});
+
+module.exports = router;
